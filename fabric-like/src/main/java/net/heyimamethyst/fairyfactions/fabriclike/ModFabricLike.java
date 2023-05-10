b196643df17375d2b197d05b08c99860d6f6b7f9
HeyImAmethyst <HeyImAmethyst44@gmail.com>
2023-05-10 12:19:08 -0400

    Fixed fairy spawn egg dispenser functionality

--- fabric-like/src/main/java/net/heyimamethyst/fairyfactions/fabriclike/ModFabricLike.java
+++ fabric-like/src/main/java/net/heyimamethyst/fairyfactions/fabriclike/ModFabricLike.java
@@ -1,15 +1,16 @@
 package net.heyimamethyst.fairyfactions.fabriclike;
 
+import net.heyimamethyst.fairyfactions.FairyConfig;
 import net.heyimamethyst.fairyfactions.FairyFactions;
 import net.heyimamethyst.fairyfactions.fabriclike.registry.ModRegistries;
 import net.heyimamethyst.fairyfactions.fabriclike.registry.ModWorldGen;
+import net.heyimamethyst.fairyfactions.items.ModSpawnEggItem;
 
 public class ModFabricLike {
     public static void init()
     {
-        FairyConfig.registerConfigs();
-        FairyConfig.passConfigValues();
         FairyFactions.init();
+        ModSpawnEggItem.InitSpawnEggs();
         ModRegistries.registerModStuffs();
         ModWorldGen.generateModWorldGen();
     }
