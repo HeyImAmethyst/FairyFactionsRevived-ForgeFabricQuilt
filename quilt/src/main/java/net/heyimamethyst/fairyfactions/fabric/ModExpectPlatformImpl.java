fb9189773b4bacdf3bdbeba1f7436ebb5173a8a2
HeyImAmethyst <HeyImAmethyst44@gmail.com>
2023-06-22 12:47:36 -0400

    refactored fabric/quilt code

--- quilt/src/main/java/net/heyimamethyst/fairyfactions/fabric/ModExpectPlatformImpl.java
+++ quilt/src/main/java/net/heyimamethyst/fairyfactions/fabric/ModExpectPlatformImpl.java
@@ -1,15 +1,24 @@
 package net.heyimamethyst.fairyfactions.fabric;
 
 import net.heyimamethyst.fairyfactions.ModExpectPlatform;
+import net.minecraft.world.entity.EntityType;
+import net.minecraft.world.item.SpawnEggItem;
 import org.quiltmc.loader.api.QuiltLoader;
 
 import java.nio.file.Path;
+import java.util.Map;
 
-public class ModExpectPlatformImpl {
+public class ModExpectPlatformImpl
+{
     /**
      * This is our actual method to {@link ModExpectPlatform#getConfigDirectory()}.
      */
     public static Path getConfigDirectory() {
         return QuiltLoader.getConfigDir();
+    }
+
+    public static Map<EntityType<?>, SpawnEggItem> getSpawnEggMap()
+    {
+        return null;
     }
 }
