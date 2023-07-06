83a768b6527486e7125abdaf69b9048a910cb7a8
HeyImAmethyst <HeyImAmethyst44@gmail.com>
2023-07-06 16:32:07 -0400

    (WIP) added functionality for fairy moods

--- fabric/src/main/java/net/heyimamethyst/fairyfactions/fabric/ModExpectPlatformImpl.java
+++ fabric/src/main/java/net/heyimamethyst/fairyfactions/fabric/ModExpectPlatformImpl.java
@@ -2,10 +2,17 @@ package net.heyimamethyst.fairyfactions.fabric;
 
 import net.heyimamethyst.fairyfactions.ModExpectPlatform;
 import net.fabricmc.loader.api.FabricLoader;
+import net.minecraft.core.Holder;
+import net.minecraft.core.Registry;
+import net.minecraft.tags.TagKey;
 import net.minecraft.world.entity.EntityType;
+import net.minecraft.world.item.Item;
 import net.minecraft.world.item.SpawnEggItem;
 
 import java.nio.file.Path;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
 import java.util.Map;
 
 public class ModExpectPlatformImpl
@@ -20,5 +27,20 @@ public class ModExpectPlatformImpl
     public static Map<EntityType<?>, SpawnEggItem> getSpawnEggMap()
     {
         return null;
+    }
+
+    public static Iterator<Item> getItemsOfTag(TagKey<Item> tag)
+    {
+        Iterable<Holder<Item>> tags = Registry.ITEM.getTagOrEmpty(tag);
+
+        List<Item> items = new ArrayList<>();
+
+        while(tags.iterator().hasNext())
+        {
+            Item item = tags.iterator().next().value();
+            items.add(item);
+        }
+
+        return items.iterator();
     }
 }
--- quilt/src/main/java/net/heyimamethyst/fairyfactions/fabric/ModExpectPlatformImpl.java
+++ quilt/src/main/java/net/heyimamethyst/fairyfactions/fabric/ModExpectPlatformImpl.java
@@ -1,12 +1,22 @@
 package net.heyimamethyst.fairyfactions.fabric;
 
+import net.fabricmc.fabric.api.tag.convention.v1.ConventionalItemTags;
+import net.fabricmc.fabric.impl.tag.convention.TagRegistration;
 import net.heyimamethyst.fairyfactions.ModExpectPlatform;
+import net.minecraft.core.Holder;
+import net.minecraft.core.HolderSet;
+import net.minecraft.core.Registry;
+import net.minecraft.tags.ItemTags;
+import net.minecraft.tags.Tag;
+import net.minecraft.tags.TagKey;
+import net.minecraft.tags.TagLoader;
 import net.minecraft.world.entity.EntityType;
+import net.minecraft.world.item.Item;
 import net.minecraft.world.item.SpawnEggItem;
 import org.quiltmc.loader.api.QuiltLoader;
 
 import java.nio.file.Path;
-import java.util.Map;
+import java.util.*;
 
 public class ModExpectPlatformImpl
 {
@@ -20,5 +30,20 @@ public class ModExpectPlatformImpl
     public static Map<EntityType<?>, SpawnEggItem> getSpawnEggMap()
     {
         return null;
+    }
+
+    public static Iterator<Item> getItemsOfTag(TagKey<Item> tag)
+    {
+        Iterable<Holder<Item>> tags = Registry.ITEM.getTagOrEmpty(tag);
+
+        List<Item> items = new ArrayList<>();
+
+        while(tags.iterator().hasNext())
+        {
+            Item item = tags.iterator().next().value();
+            items.add(item);
+        }
+
+        return items.iterator();
     }
 }
