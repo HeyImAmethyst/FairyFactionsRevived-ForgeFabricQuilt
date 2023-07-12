999f275c3878922e6d246c3d191de092d23fd601
HeyImAmethyst <HeyImAmethyst44@gmail.com>
2023-07-12 09:33:55 -0400

    added cooking job

--- common/src/main/java/net/heyimamethyst/fairyfactions/registry/ModItemTags.java
+++ common/src/main/java/net/heyimamethyst/fairyfactions/registry/ModItemTags.java
@@ -42,6 +42,8 @@ public class ModItemTags
 	public static final TagKey<Item> IS_ADDITIONAL_ITEM_PICKUP = makeTag("is_additional_item_pickup");
 
 	public static final TagKey<Item> ITEM_TO_SMELT = makeTag("item_to_smelt");
+	public static final TagKey<Item> ITEM_TO_COOK = makeTag("item_to_cook");
+
 	private static TagKey<Item> bind(String p_203855_)
 	{
 		return TagKey.create(Registry.ITEM_REGISTRY, new ResourceLocation(p_203855_));
