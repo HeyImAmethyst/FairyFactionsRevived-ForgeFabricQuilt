550b68467688108ac8e84b242ebae432d10f1a2e
HeyImAmethyst <HeyImAmethyst44@gmail.com>
2023-07-22 17:04:11 -0400

    fixed errors related to 1.19.3 changes

--- common/src/main/java/net/heyimamethyst/fairyfactions/client/render/entity/FairyBedRenderer.java
+++ common/src/main/java/net/heyimamethyst/fairyfactions/client/render/entity/FairyBedRenderer.java
@@ -4,7 +4,7 @@ import com.google.common.collect.ImmutableMap;
 import com.mojang.blaze3d.vertex.PoseStack;
 import com.mojang.blaze3d.vertex.VertexConsumer;
 import com.mojang.datafixers.util.Pair;
-import com.mojang.math.Vector3f;
+import com.mojang.math.Axis;
 import net.heyimamethyst.fairyfactions.FairyFactions;
 import net.heyimamethyst.fairyfactions.client.model.ModModelLayers;
 import net.heyimamethyst.fairyfactions.client.model.entity.FairyBedModel;
@@ -41,7 +41,7 @@ public class FairyBedRenderer extends EntityRenderer<FairyBedEntity>
         //poseStack.translate(0.0, 0.375, 0.0);
         poseStack.translate(0.0, -0.525, 0.0);
 
-        poseStack.mulPose(Vector3f.YP.rotationDegrees(180.0f - f));
+        poseStack.mulPose(Axis.YP.rotationDegrees(180.0f - f));
         float h = (float)fairyBed.getHurtTime() - g;
         float j = fairyBed.getDamage() - g;
 
@@ -52,7 +52,7 @@ public class FairyBedRenderer extends EntityRenderer<FairyBedEntity>
 
         if (h > 0.0f)
         {
-            poseStack.mulPose(Vector3f.XP.rotationDegrees(Mth.sin(h) * h * j / 10.0f * (float)fairyBed.getHurtDir()));
+            poseStack.mulPose(Axis.XP.rotationDegrees(Mth.sin(h) * h * j / 10.0f * (float)fairyBed.getHurtDir()));
         }
 
         Pair<ResourceLocation, FairyBedModel> pair = this.fairyBedResources.get((Object)fairyBed.getFairyBedType());
@@ -60,7 +60,7 @@ public class FairyBedRenderer extends EntityRenderer<FairyBedEntity>
         FairyBedModel fairyBedModel = pair.getSecond();
 
         //poseStack.scale(-1.0f, -1.0f, 1.0f);
-        poseStack.mulPose(Vector3f.YP.rotationDegrees(180.0f));
+        poseStack.mulPose(Axis.YP.rotationDegrees(180.0f));
         //fairyBedModel.setupAnim(fairyBed, g, 0.0f, -0.1f, 0.0f, 0.0f);
 
         VertexConsumer vertexConsumer = multiBufferSource.getBuffer(fairyBedModel.renderType(resourceLocation));
