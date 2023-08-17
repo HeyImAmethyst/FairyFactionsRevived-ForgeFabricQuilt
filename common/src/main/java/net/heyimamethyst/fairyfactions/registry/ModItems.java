c8ab857834a510b104b3b243eb38a7061ca3f10b
HeyImAmethyst <HeyImAmethyst44@gmail.com>
2023-08-17 14:23:45 -0400

    made some changes to make it easier to add new fairy bed varients

--- common/src/main/java/net/heyimamethyst/fairyfactions/registry/ModItems.java
+++ common/src/main/java/net/heyimamethyst/fairyfactions/registry/ModItems.java
@@ -3,14 +3,19 @@ package net.heyimamethyst.fairyfactions.registry;
 import dev.architectury.registry.registries.DeferredRegister;
 import dev.architectury.registry.registries.RegistrySupplier;
 import net.heyimamethyst.fairyfactions.FairyFactions;
+import net.heyimamethyst.fairyfactions.client.model.ModModelLayers;
 import net.heyimamethyst.fairyfactions.entities.FairyBedEntity;
 import net.heyimamethyst.fairyfactions.items.FairyBedItem;
 import net.heyimamethyst.fairyfactions.items.ModSpawnEggItem;
+import net.minecraft.client.model.geom.ModelLayerLocation;
 import net.minecraft.core.Registry;
 import net.minecraft.core.registries.Registries;
 import net.minecraft.world.item.CreativeModeTab;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.SpawnEggItem;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class ModItems
 {
@@ -27,51 +32,27 @@ public class ModItems
     public static final RegistrySupplier<Item> FAIRY_SPAWN_EGG = ITEMS.register("fairy_spawn_egg", () ->
             new ModSpawnEggItem(ModEntities.FAIRY_ENTITY, 0xea8fde, 0x8658bf, new Item.Properties()));
 
-    public static final RegistrySupplier<Item> OAK_WHITE_FAIRY_BED = ITEMS.register("oak_white_fairy_bed", () ->
-            new FairyBedItem(FairyBedEntity.Type.OAK_WHITE, new Item.Properties().stacksTo(1)));
+    public static final Map<FairyBedEntity.Type, RegistrySupplier<Item>> FAIRY_BED_ITEMS = creatFairyBedItemMap();
 
-    public static final RegistrySupplier<Item> OAK_ORANGE_FAIRY_BED = ITEMS.register("oak_orange_fairy_bed", () ->
-            new FairyBedItem(FairyBedEntity.Type.OAK_ORANGE, new Item.Properties().stacksTo(1)));
+    private static Map<FairyBedEntity.Type, RegistrySupplier<Item>> creatFairyBedItemMap()
+    {
+        Map<FairyBedEntity.Type, RegistrySupplier<Item>> map = new HashMap<>();
 
-    public static final RegistrySupplier<Item> OAK_MAGENTA_FAIRY_BED = ITEMS.register("oak_magenta_fairy_bed", () ->
-            new FairyBedItem(FairyBedEntity.Type.OAK_MAGENTA, new Item.Properties().stacksTo(1)));
+        for (FairyBedEntity.Type type : FairyBedEntity.Type.values())
+        {
+            map.put(type, createFairyBedName(type));
+        }
 
-    public static final RegistrySupplier<Item> OAK_LIGHT_BLUE_FAIRY_BED = ITEMS.register("oak_light_blue_fairy_bed", () ->
-            new FairyBedItem(FairyBedEntity.Type.OAK_LIGHT_BLUE, new Item.Properties().stacksTo(1)));
+        return map;
+    }
 
-    public static final RegistrySupplier<Item> OAK_YELLOW_FAIRY_BED = ITEMS.register("oak_yellow_fairy_bed", () ->
-            new FairyBedItem(FairyBedEntity.Type.OAK_YELLOW, new Item.Properties().stacksTo(1)));
+    public static RegistrySupplier<Item> createFairyBedName(FairyBedEntity.Type type)
+    {
+        return ITEMS.register(type.getName() + "_fairy_bed", () -> createFairyBed(type));
+    }
 
-    public static final RegistrySupplier<Item> OAK_LIME_FAIRY_BED = ITEMS.register("oak_lime_fairy_bed", () ->
-            new FairyBedItem(FairyBedEntity.Type.OAK_LIME, new Item.Properties().stacksTo(1)));
-
-    public static final RegistrySupplier<Item> OAK_PINK_FAIRY_BED = ITEMS.register("oak_pink_fairy_bed", () ->
-            new FairyBedItem(FairyBedEntity.Type.OAK_PINK, new Item.Properties().stacksTo(1)));
-
-    public static final RegistrySupplier<Item> OAK_GRAY_FAIRY_BED = ITEMS.register("oak_gray_fairy_bed", () ->
-            new FairyBedItem(FairyBedEntity.Type.OAK_GRAY, new Item.Properties().stacksTo(1)));
-
-    public static final RegistrySupplier<Item> OAK_LIGHT_GRAY_FAIRY_BED = ITEMS.register("oak_light_gray_fairy_bed", () ->
-            new FairyBedItem(FairyBedEntity.Type.OAK_LIGHT_GRAY, new Item.Properties().stacksTo(1)));
-
-    public static final RegistrySupplier<Item> OAK_CYAN_FAIRY_BED = ITEMS.register("oak_cyan_fairy_bed", () ->
-            new FairyBedItem(FairyBedEntity.Type.OAK_CYAN, new Item.Properties().stacksTo(1)));
-
-    public static final RegistrySupplier<Item> OAK_PURPLE_FAIRY_BED = ITEMS.register("oak_purple_fairy_bed", () ->
-            new FairyBedItem(FairyBedEntity.Type.OAK_PURPLE, new Item.Properties().stacksTo(1)));
-
-    public static final RegistrySupplier<Item> OAK_BLUE_FAIRY_BED = ITEMS.register("oak_blue_fairy_bed", () ->
-            new FairyBedItem(FairyBedEntity.Type.OAK_BLUE, new Item.Properties().stacksTo(1)));
-
-    public static final RegistrySupplier<Item> OAK_BROWN_FAIRY_BED = ITEMS.register("oak_brown_fairy_bed", () ->
-            new FairyBedItem(FairyBedEntity.Type.OAK_BROWN, new Item.Properties().stacksTo(1)));
-
-    public static final RegistrySupplier<Item> OAK_GREEN_FAIRY_BED = ITEMS.register("oak_green_fairy_bed", () ->
-            new FairyBedItem(FairyBedEntity.Type.OAK_GREEN, new Item.Properties().stacksTo(1)));
-
-    public static final RegistrySupplier<Item> OAK_RED_FAIRY_BED = ITEMS.register("oak_red_fairy_bed", () ->
-            new FairyBedItem(FairyBedEntity.Type.OAK_RED, new Item.Properties().stacksTo(1)));
-
-    public static final RegistrySupplier<Item> OAK_BLACK_FAIRY_BED = ITEMS.register("oak_black_fairy_bed", () ->
-            new FairyBedItem(FairyBedEntity.Type.OAK_BLACK, new Item.Properties().stacksTo(1)));
+    private static Item createFairyBed(FairyBedEntity.Type bedType)
+    {
+        return new FairyBedItem(bedType, new Item.Properties().stacksTo(1));
+    }
 }
