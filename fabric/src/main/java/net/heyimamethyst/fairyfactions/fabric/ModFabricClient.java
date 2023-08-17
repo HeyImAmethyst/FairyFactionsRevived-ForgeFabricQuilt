95df24e4970bd208c0fe60592ebbc038d01e79d2
HeyImAmethyst <HeyImAmethyst44@gmail.com>
2023-08-17 13:44:53 -0400

    cleaned up imports in ModFabricClient

--- fabric/src/main/java/net/heyimamethyst/fairyfactions/fabric/ModFabricClient.java
+++ fabric/src/main/java/net/heyimamethyst/fairyfactions/fabric/ModFabricClient.java
@@ -1,18 +1,7 @@
 package net.heyimamethyst.fairyfactions.fabric;
 
-import dev.architectury.registry.client.level.entity.EntityModelLayerRegistry;
-import dev.architectury.registry.client.level.entity.EntityRendererRegistry;
 import net.fabricmc.api.ClientModInitializer;
-import net.heyimamethyst.fairyfactions.client.model.ModModelLayers;
-import net.heyimamethyst.fairyfactions.client.model.entity.FairyEyesModel;
-import net.heyimamethyst.fairyfactions.client.model.entity.FairyModel;
-import net.heyimamethyst.fairyfactions.client.model.entity.FairyProps2Model;
-import net.heyimamethyst.fairyfactions.client.model.entity.FairyPropsModel;
-import net.heyimamethyst.fairyfactions.client.render.entity.FairyFishHookEntityRenderer;
-import net.heyimamethyst.fairyfactions.client.render.entity.FairyRenderer;
 import net.heyimamethyst.fairyfactions.fabriclike.ModFabricLikeClient;
-import net.heyimamethyst.fairyfactions.fabriclike.registry.KeyInputHandler;
-import net.heyimamethyst.fairyfactions.registry.ModEntities;
 
 public class ModFabricClient implements ClientModInitializer
 {
