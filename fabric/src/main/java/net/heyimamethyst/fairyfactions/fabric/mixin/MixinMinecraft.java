069a4aa33205096eae44cbfe6da4acb60c07b18d
HeyImAmethyst <HeyImAmethyst44@gmail.com>
2023-09-25 09:13:09 -0400

    added data gen to auto generate json files added new fairy bed varients

---
+++ fabric/src/main/java/net/heyimamethyst/fairyfactions/fabric/mixin/MixinMinecraft.java
@@ -0,0 +1,31 @@
+package net.heyimamethyst.fairyfactions.fabric.mixin;
+
+import net.fabricmc.fabric.api.resource.ResourceManagerHelper;
+import net.heyimamethyst.fairyfactions.client.texture.fairy_bed_texture.FairyBedTextureGenerator;
+import net.heyimamethyst.fairyfactions.fabriclike.ReloadListenerWrapper;
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.main.GameConfig;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.server.packs.PackType;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(Minecraft.class)
+public abstract class MixinMinecraft
+{
+
+    //@Inject(at = @At(value = "INVOKE_ASSIGN", target = "Lnet/minecraft/client/resources/model/ModelBakery;getBakedTopLevelModels()Ljava/util/Map;", shift = At.Shift.AFTER), method = "apply(Lnet/minecraft/client/resources/model/ModelBakery;Lnet/minecraft/server/packs/resources/ResourceManager;Lnet/minecraft/util/profiling/ProfilerFiller;)V")
+    @Inject(method = "<init>", at = @At( value = "INVOKE",
+            target = "Lnet/minecraft/client/resources/PaintingTextureManager;<init>(Lnet/minecraft/client/renderer/texture/TextureManager;)V"))
+    public void insertFairyBedTextureGenerator(GameConfig gameConfig, CallbackInfo callbackInfo)
+    {
+        ResourceManagerHelper resourceManagerHelper = ResourceManagerHelper.get(PackType.CLIENT_RESOURCES);
+        resourceManagerHelper.registerReloadListener(new ReloadListenerWrapper<>(
+                new ResourceLocation("fairyfactions", "fairy_bed_texture_generator"),
+                FairyBedTextureGenerator.INSTANCE
+        ));
+    }
+
+}
---
+++ forge/src/main/java/net/heyimamethyst/fairyfactions/forge/datagen/ModRecipeProvider.java
@@ -0,0 +1,72 @@
+package net.heyimamethyst.fairyfactions.forge.datagen;
+
+import com.google.common.collect.ImmutableList;
+import com.google.common.collect.Lists;
+import com.google.gson.JsonObject;
+import dev.architectury.registry.registries.RegistrySupplier;
+import net.heyimamethyst.fairyfactions.entities.FairyBedEntity;
+import net.heyimamethyst.fairyfactions.items.FairyBedItem;
+import net.heyimamethyst.fairyfactions.registry.ModItems;
+import net.minecraft.advancements.critereon.ItemPredicate;
+import net.minecraft.core.Holder;
+import net.minecraft.core.Registry;
+import net.minecraft.data.DataGenerator;
+import net.minecraft.data.recipes.*;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.tags.ItemTags;
+import net.minecraft.tags.TagKey;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.Items;
+import net.minecraft.world.item.crafting.Ingredient;
+import net.minecraft.world.level.ItemLike;
+import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.Blocks;
+import net.minecraft.world.level.block.state.properties.WoodType;
+import net.minecraftforge.common.crafting.conditions.IConditionBuilder;
+import net.minecraftforge.registries.ForgeRegistries;
+
+import java.util.Collection;
+import java.util.List;
+import java.util.function.Consumer;
+import java.util.stream.Stream;
+
+public class ModRecipeProvider extends RecipeProvider implements IConditionBuilder
+{
+
+	public ModRecipeProvider(DataGenerator p_125973_)
+	{
+		super(p_125973_);
+	}
+
+	@Override
+	protected void buildCraftingRecipes(Consumer<FinishedRecipe> finishedRecipeConsumer)
+	{
+
+		AddFairyBedRecipies(finishedRecipeConsumer);
+
+	}
+
+	private static void AddFairyBedRecipies(Consumer<FinishedRecipe> finishedRecipeConsumer)
+	{
+		for (RegistrySupplier<FairyBedItem> supplier: ModItems.FAIRY_BED_ITEMS.values())
+		{
+			fairyBedBuilder(supplier.get(), supplier.get().getLogType(), supplier.get().getWoolType()).unlockedBy("has_log", has(ItemTags.LOGS)).save(finishedRecipeConsumer);
+		}
+	}
+
+	protected static RecipeBuilder fairyBedBuilder(ItemLike itemResult, FairyBedEntity.LogType logType, FairyBedEntity.WoolType woolType)
+	{
+		return ShapedRecipeBuilder.shaped(itemResult, 1).group("fairy_bed")
+				.define('#', ForgeRegistries.ITEMS.getValue(new ResourceLocation(logType.getName() + "_log")))
+				.define('W', ForgeRegistries.ITEMS.getValue(new ResourceLocation(woolType.getName() + "_wool")))
+				.define('G', Items.GLOWSTONE)
+				.pattern("###")
+				.pattern("#G#")
+				.pattern("#W#");
+	}
+	
+	protected static SimpleCookingRecipeBuilder cook(ItemLike itemResult, Ingredient ingredient)
+	{
+		return SimpleCookingRecipeBuilder.smelting(ingredient, itemResult, 0.7F, 200).unlockedBy(getHasName(itemResult), has(itemResult));
+	}
+}
