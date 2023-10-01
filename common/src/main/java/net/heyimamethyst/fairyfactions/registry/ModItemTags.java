22768c4f55f1e44300205c0d3966b3847deaa827
HeyImAmethyst <HeyImAmethyst44@gmail.com>
2023-09-30 22:39:40 -0400

    made adjustments to smelting and cooking jobs

--- common/src/main/java/net/heyimamethyst/fairyfactions/registry/ModItemTags.java
+++ common/src/main/java/net/heyimamethyst/fairyfactions/registry/ModItemTags.java
@@ -3,7 +3,6 @@ package net.heyimamethyst.fairyfactions.registry;
 import net.heyimamethyst.fairyfactions.FairyFactions;
 import net.minecraft.core.Registry;
 import net.minecraft.resources.ResourceLocation;
-import net.minecraft.tags.ItemTags;
 import net.minecraft.tags.TagKey;
 import net.minecraft.world.item.Item;
 
@@ -41,8 +40,8 @@ public class ModItemTags
 
 	public static final TagKey<Item> IS_ADDITIONAL_ITEM_PICKUP = makeTag("is_additional_item_pickup");
 
-	public static final TagKey<Item> ITEM_TO_SMELT = makeTag("item_to_smelt");
-	public static final TagKey<Item> ITEM_TO_COOK = makeTag("item_to_cook");
+	public static final TagKey<Item> INGOTS_AND_GEMS = makeTag("ingots_and_gems");
+	//public static final TagKey<Item> ITEM_TO_COOK = makeTag("item_to_cook");
 
 	private static TagKey<Item> bind(String p_203855_)
 	{
