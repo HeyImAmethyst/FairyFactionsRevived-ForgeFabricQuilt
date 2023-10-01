ddbebe59f6552db7e354697b412c4ed614e88692
HeyImAmethyst <HeyImAmethyst44@gmail.com>
2023-09-30 23:23:11 -0400

    fixed errors in smelting/cooking jobs

--- common/src/main/java/net/heyimamethyst/fairyfactions/entities/ai/fairy_job/JobCook.java
+++ common/src/main/java/net/heyimamethyst/fairyfactions/entities/ai/fairy_job/JobCook.java
@@ -145,7 +145,7 @@ public class JobCook extends FairyJob
 
             for (SmokingRecipe recipe: list)
             {
-                if(inputHasRecipe(recipe, stack, i))
+                if(inputHasRecipe(world, recipe, stack, i))
                 {
                     ItemStack furnaceStack = smoker.getItem(0);
                     ItemStack itemStackForFurnace = new ItemStack(stack.getItem());
@@ -167,14 +167,14 @@ public class JobCook extends FairyJob
         return false;
     }
 
-    private static boolean inputHasRecipe(@Nullable Recipe<?> recipe, ItemStack input, int i)
+    private static boolean inputHasRecipe(Level level, @Nullable Recipe<?> recipe, ItemStack input, int i)
     {
         if (input.isEmpty() || recipe == null)
         {
             return false;
         }
 
-        ItemStack itemStack = recipe.getResultItem();
+        ItemStack itemStack = recipe.getResultItem(level.registryAccess());
 
         if (itemStack.isEmpty())
         {
